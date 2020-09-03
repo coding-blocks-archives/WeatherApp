1ce83d09df68cf0c31ef9d3f82da9117e5cf9a14
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-09-03 13:59:21 +0530

    add bottom bar

---
+++ app/src/main/java/com/codingblocks/weatherapp/ui/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.codingblocks.weatherapp.ui
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/codingblocks/weatherapp/ui/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.codingblocks.weatherapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.codingblocks.weatherapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val homeFragment = HomeFragment()
+    private val fragmentManager = supportFragmentManager
+    private var activeFragment: Fragment = homeFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fragmentManager.beginTransaction().apply {
+            add(R.id.container, homeFragment, getString(R.string.home_dashboard))
+        }.commit()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.homeFragment -> {
+                    fragmentManager.beginTransaction().hide(activeFragment).show(homeFragment)
+                        .commit()
+                    activeFragment = homeFragment
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+}
+
